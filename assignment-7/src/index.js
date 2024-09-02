4f02e4a22820af0cae119b11e6d2b76c7aa07413
NukulKumawat <nukulkumawat@gmail.com>
2024-09-02 20:26:10 +0530

    Assignment 7

---
+++ assignment-7/src/index.js
@@ -0,0 +1,55 @@
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import app from './app.js'
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+.then(()=>{
+    /*app.on("error",(error)=>{
+        console.log(`ERRRRR: `,error);
+        throw error
+    })*/
+    app.listen(process.env.port || 8000,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed! ",err)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /*const app= express()
+
+(async ()=>{
+    try {
+        await mongoose.connect(`process.env.${MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERRRR: ",error);
+            throw error
+        })
+    } catch (error) {
+        console.error("ERROR: ",error)
+        throw err
+    }
+})()*/
